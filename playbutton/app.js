c5369773a8462d6ee1f8487f68df7511e78e7c59
gabdigi <gabrieldigiart@gmail.com>
2021-10-24 20:44:15 +0200

    push

--- playbutton/app.js
+++ playbutton/app.js
@@ -1,43 +1,75 @@
-let clap = new Audio("sound/Clap-Baby.ogg");
-let keyA    = document.getElementById('clapsound');
+let clap    = new Audio("sound/Clap-Baby.ogg");
+let kick    = new Audio("sound/Kick-Base.ogg");
+let snare   = new Audio("sound/Snare-Young-Thug.ogg");
+let hihat   = new Audio("sound/Hihat-Short-Closed.ogg");
+let keyA    = document.querySelector('#keyA');
+let keyZ    = document.querySelector('#keyZ');
+let keyE    = document.querySelector('#keyE');
+let keyR    = document.querySelector('#keyR');
 
-keyA.addEventListener('onkeyup', function(e){
+
+document.addEventListener('keyup', function(e){
     if (e.keyCode == 65){
-        console("ok");
         clap.play();
+        keyA.style.borderColor = "grey";
+        keyA.style.borderRadius = '5px';
     }
-    console("nope");
-})
+    else if (e.keyCode == 90){
+        kick.play();
+        keyZ.style.borderColor = "grey";
+        keyZ.style.borderRadius = '5px';
+    }
+    else if (e.keyCode == 69){
+        snare.play();
+        keyE.style.borderColor = "grey";
+        keyE.style.borderRadius = '5px';
+    }
+    else if (e.keyCode == 82){
+        hihat.play();
+        keyR.style.borderColor = "grey";
+        keyR.style.borderRadius = '5px';
+    }
+     else{
+         keyA.style.borderColor = "grey";
+         keyZ.style.borderColor = "grey";
+         keyE.style.borderColor = "grey";
+         keyR.style.borderColor = "grey";
 
-/**
- * 
- * let clap = new Audio;
-clap.src = "sound/Clap-Baby.ogg";
-//clap.webkit-playsinline="true";
-clap.playsinline="true";
- function test() {
-    clap.play();
-    console.log("coucou");
-  }
+    }
+    
+});
 
-test();
-
-methode 2 
-let clap = new Audio("sound/Clap-Baby.ogg");
-//clap.webkit-playsinline="true";
-clap.playsinline="true";
-
-document.addEventListener('keydown', function(e) {
-    if (e.keyCode == 65) {
+document.addEventListener('keydown', function(e){
+    if (e.keyCode == 65){
         clap.play();
+        keyA.style.borderColor = "red";
+        keyA.style.borderRadius = '5px';
+    } 
+    else if (e.keyCode == 90){
+        kick.play();
+        keyZ.style.borderColor = "purple";
+        keyZ.style.borderRadius = '5px';
     }
-  });
+    else if (e.keyCode == 69){
+        snare.play();
+        keyE.style.borderColor = "turquoise";
+        keyE.style.borderRadius = '5px';
+    }
+    else if (e.keyCode == 82){
+        hihat.play();
+        keyR.style.borderColor = "yellow";
+        keyR.style.borderRadius = '5px';
+    }
+    else{
+         keyA.style.borderColor = "grey";
+         keyZ.style.borderColor = "grey";
+         keyE.style.borderColor = "grey";
+         keyR.style.borderColor = "grey";
+    }
+    
+});
 
-methode 3
-let clap = new Audio("sound/Clap-Baby.ogg");
-clap.playsinline="true";
- function test() {
-    clap.play();
-    console.log("coucou");
-}
- */
+
+
+
+
